783417f25d13a842a63046f9cd118137fa8222c3
Rebecca Parks <rparkstx@gmail.com>
2024-01-29 12:23:54 -0500

    Add error checking for trying to check out a checked out vehicle

--- client/src/components/CheckoutPage/CheckoutPage.jsx
+++ client/src/components/CheckoutPage/CheckoutPage.jsx
@@ -2,11 +2,13 @@ import { useEffect, useState, useContext } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import UserContext from "../../context/UserContext";
+import { getVehicleDetails } from "../../assets/helpers";
 
 export default function CheckoutPage() {
     const params = useParams();
     const { userData } = useContext(UserContext);
     const vehicleNumber = params.vehicleNumber;
+    const [vehicle, setVehicle] = useState({});
 
     const [destination, setDestination] = useState("");
 
@@ -35,56 +37,57 @@ export default function CheckoutPage() {
     }
 
     useEffect(() => {
-        getVehicleDetails(vehicleNumber);
+        getVehicleDetails(vehicleNumber).then((vehicle) => setVehicle(vehicle));
     }, []);
-
-    async function getVehicleDetails(vehicleNumber) {
-        // TODO
-        const url = "http://localhost:8081/api/vehicles/" + vehicleNumber;
-        try {
-            const vehicle = await axios.get(url);
-            console.log(vehicle);
-        } catch (err) {
-            console.log("Vehicle not found");
-        }
-    }
 
     return (
         <div className="d-flex justify-content-center">
             <div className="w-25">
                 <h2 className="text-center mb-3">Checkout</h2>
-                <form onSubmit={submitHandler}>
-                    <div className="mb-3">
-                        <label className="form-label">Vehicle Number</label>
-                        <input
-                            disabled
-                            value={vehicleNumber}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Employee</label>
-                        <input
-                            disabled
-                            value={userData.user.username}
-                            className="form-control"
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label className="form-label">Destination</label>
-                        <input
-                            autoFocus
-                            value={destination}
-                            onChange={destinationChangeHander}
-                            type="text"
-                            className="form-control"
-                            placeholder="STEM I"
-                        />
-                    </div>
-
-                    <button className="btn btn-primary">Submit</button>
-                </form>
+                {vehicle.checkedOut ? (
+                    <p className="text-center">
+                        Error: Vehicle is already checked out.
+                    </p>
+                ) : (
+                    <CheckoutForm />
+                )}
             </div>
         </div>
     );
+
+    function CheckoutForm() {
+        return (
+            <form onSubmit={submitHandler}>
+                <div className="mb-3">
+                    <label className="form-label">Vehicle Number</label>
+                    <input
+                        disabled
+                        value={vehicleNumber}
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Employee</label>
+                    <input
+                        disabled
+                        value={userData.user.username}
+                        className="form-control"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Destination</label>
+                    <input
+                        autoFocus
+                        value={destination}
+                        onChange={destinationChangeHander}
+                        type="text"
+                        className="form-control"
+                        placeholder="STEM I"
+                    />
+                </div>
+
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        );
+    }
 }

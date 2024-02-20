19593aaecf8ec05d7b4411fdf29be446d3d83b82
Rebecca Parks <rparkstx@gmail.com>
2024-02-20 16:11:36 -0500

    Enable destination export/import

--- client/src/components/DestinationsPage/DestinationsPage.jsx
+++ client/src/components/DestinationsPage/DestinationsPage.jsx
@@ -20,14 +20,14 @@ export default function DestinationsPage({ isAdmin, mode }) {
         refreshDestinations();
     }, []);
 
-    // function exportDestinationsHandler(event) {
-    //     event.preventDefault();
-    //     const destinationsString = JSON.stringify(destinations);
-    //     const now = new Date(Date.now()).toISOString();
-    //     const fileName = "CarBoy_destinations_" + now + ".json";
+    function exportDestinationsHandler(event) {
+        event.preventDefault();
+        const destinationsString = JSON.stringify(destinations);
+        const now = new Date(Date.now()).toISOString();
+        const fileName = "CarBoy_destinations_" + now + ".json";
 
-    //     downloadJSONFile(fileName, destinationsString);
-    // }
+        downloadJSONFile(fileName, destinationsString);
+    }
 
     async function importDestinationsHandler(event) {
         event.preventDefault();
@@ -89,7 +89,7 @@ export default function DestinationsPage({ isAdmin, mode }) {
                         </svg>
                         Add Destination
                     </Link>
-                    {/* <button
+                    <button
                         className="btn btn-secondary me-1"
                         onClick={exportDestinationsHandler}
                     >
@@ -105,7 +105,7 @@ export default function DestinationsPage({ isAdmin, mode }) {
                             <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
                         </svg>
                         Export
-                    </button> */}
+                    </button>
                     <button
                         className="btn btn-secondary me-1"
                         onClick={importDestinationsHandler}
@@ -121,7 +121,7 @@ export default function DestinationsPage({ isAdmin, mode }) {
                             <path d="M8.5 11.5a.5.5 0 0 1-1 0V7.707L6.354 8.854a.5.5 0 1 1-.708-.708l2-2a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 7.707z" />
                             <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
                         </svg>
-                        Import CSV
+                        Import
                     </button>
                 </div>
                 {error && <p className="text-center text-danger">{error}</p>}

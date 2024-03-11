7d0e880196e8b1a426677bd668951ec4b5dbea96
Rebecca Parks <rparkstx@gmail.com>
2024-03-11 09:39:59 -0400

    Add vehicle number to trip export

--- client/src/components/TripsPage/TripsPage.jsx
+++ client/src/components/TripsPage/TripsPage.jsx
@@ -95,6 +95,7 @@ export default function TripsPage() {
             trip.employee = trip.employee.fullName
                 ? trip.employee.fullName
                 : trip.employee.username;
+            trip.vehicleNumber = vehicleNumber;
         }
     }
 
@@ -108,7 +109,7 @@ export default function TripsPage() {
                     <div>
                         <div className="d-flex justify-content-center mb-3">
                             <button
-                                className="btn btn-secondary me-1"
+                                className="btn btn-primary me-1"
                                 onClick={exportAllHandler}
                             >
                                 <svg
@@ -122,25 +123,48 @@ export default function TripsPage() {
                                     <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
                                     <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
                                 </svg>
-                                Export CSV (All)
+                                Export CSV
                             </button>
                             <button
-                                className="btn btn-secondary me-1"
-                                onClick={exportFilteredHandler}
+                                className="btn btn-outline-secondary me-1"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#filterBar"
+                                aria-expanded="false"
+                                aria-controls="filterBar"
                             >
                                 <svg
                                     xmlns="http://www.w3.org/2000/svg"
                                     width="16"
                                     height="16"
                                     fill="currentColor"
-                                    className="bi bi-file-earmark-arrow-down"
+                                    className="bi bi-filter"
                                     viewBox="0 0 16 16"
                                 >
-                                    <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
-                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                                    <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5m-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
                                 </svg>
-                                Export CSV (Filtered)
+                                Filter
                             </button>
+                        </div>
+                        <div className="collapse" id="filterBar">
+                            <div className="d-flex justify-content-center mb-3">
+                                <button
+                                    className="btn btn-secondary me-1"
+                                    onClick={exportFilteredHandler}
+                                >
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+                                        className="bi bi-file-earmark-arrow-down"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                                    </svg>
+                                    Export Filtered CSV
+                                </button>
+                            </div>
                         </div>
                         <div
                             className={

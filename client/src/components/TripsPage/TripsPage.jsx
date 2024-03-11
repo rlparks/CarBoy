066c6b05b08b425aaade4667784d22128c76d752
Rebecca Parks <rparkstx@gmail.com>
2024-03-11 12:46:05 -0400

    Finish filter styling

--- client/src/components/TripsPage/TripsPage.jsx
+++ client/src/components/TripsPage/TripsPage.jsx
@@ -29,6 +29,9 @@ export default function TripsPage() {
     const [filteredTrips, setFilteredTrips] = useState("");
     const { userData } = useContext(UserContext);
 
+    const [startFilter, setStartFilter] = useState("");
+    const [endFilter, setEndFilter] = useState("");
+
     useEffect(() => {
         getVehicleDetails(vehicleNumber, userData.token).then(
             async (vehicle) => {
@@ -49,6 +52,10 @@ export default function TripsPage() {
             }
         );
     }, []);
+
+    useEffect(() => {
+        console.log("Filter changed!");
+    }, [startFilter, endFilter]);
 
     async function exportAllHandler(event) {
         event.preventDefault();
@@ -146,24 +153,76 @@ export default function TripsPage() {
                             </button>
                         </div>
                         <div className="collapse" id="filterBar">
-                            <div className="d-flex justify-content-center mb-3">
-                                <button
-                                    className="btn btn-secondary me-1"
-                                    onClick={exportFilteredHandler}
-                                >
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        width="16"
-                                        height="16"
-                                        fill="currentColor"
-                                        className="bi bi-file-earmark-arrow-down"
-                                        viewBox="0 0 16 16"
-                                    >
-                                        <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
-                                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
-                                    </svg>
-                                    Export Filtered CSV
-                                </button>
+                            <div className="">
+                                <div className="row">
+                                    <div className="d-flex justify-content-center mb-3">
+                                        <div className="col">
+                                            <div className="input-group">
+                                                <span
+                                                    id="startingFrom"
+                                                    className="input-group-text"
+                                                >
+                                                    Starting from:
+                                                </span>
+                                                <input
+                                                    className="form-control me-1"
+                                                    type="date"
+                                                    aria-describedby="startingFrom"
+                                                    value={startFilter}
+                                                    onChange={(e) =>
+                                                        setStartFilter(
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <div className="input-group">
+                                                <span
+                                                    id="endingOn"
+                                                    className="input-group-text"
+                                                >
+                                                    Ending on:
+                                                </span>
+                                                <input
+                                                    className="form-control me-1"
+                                                    type="date"
+                                                    aria-describedby="endingOn"
+                                                    value={endFilter}
+                                                    onChange={(e) =>
+                                                        setEndFilter(
+                                                            e.target.value
+                                                        )
+                                                    }
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="col">
+                                            <div className="d-flex justify-content-center">
+                                                <button
+                                                    className="btn btn-secondary"
+                                                    onClick={
+                                                        exportFilteredHandler
+                                                    }
+                                                >
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        width="16"
+                                                        height="16"
+                                                        fill="currentColor"
+                                                        className="bi bi-file-earmark-arrow-down"
+                                                        viewBox="0 0 16 16"
+                                                    >
+                                                        <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                                                        <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                                                    </svg>
+                                                    Export Filtered CSV
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                         <div

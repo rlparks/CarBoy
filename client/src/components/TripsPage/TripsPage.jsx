66dc6cf57925c4b0dda77a9ad89a7536dd7e1f8c
Rebecca Parks <rparkstx@gmail.com>
2024-03-11 13:45:48 -0400

    Add clear filter

--- client/src/components/TripsPage/TripsPage.jsx
+++ client/src/components/TripsPage/TripsPage.jsx
@@ -143,6 +143,12 @@ export default function TripsPage() {
         }
     }
 
+    function clearFilter(event) {
+        event.preventDefault();
+        setStartFilter("");
+        setEndFilter("");
+    }
+
     return vehicle ? (
         <div className="d-flex justify-content-center">
             <div className="w-50">
@@ -235,6 +241,23 @@ export default function TripsPage() {
                                     </div>
                                     <div className="col">
                                         <div className="d-flex justify-content-center">
+                                            <button
+                                                className="btn btn-outline-secondary me-1"
+                                                onClick={clearFilter}
+                                            >
+                                                <svg
+                                                    xmlns="http://www.w3.org/2000/svg"
+                                                    width="16"
+                                                    height="16"
+                                                    fill="currentColor"
+                                                    className="bi bi-file-earmark-arrow-down"
+                                                    viewBox="0 0 16 16"
+                                                >
+                                                    <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z" />
+                                                    <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z" />
+                                                </svg>
+                                                Clear Filter
+                                            </button>
                                             <button
                                                 className="btn btn-secondary"
                                                 onClick={exportFilteredHandler}

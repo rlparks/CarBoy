a093678521af0c1b58d8cc864b7b1bcf5c0aa0de
Rebecca Parks <rparkstx@gmail.com>
2024-03-28 13:51:10 -0400

    Move "delete user" button inside EditUser (admin)

--- client/src/components/EditUser/EditUser.jsx
+++ client/src/components/EditUser/EditUser.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { getUser } from "../../assets/helpers";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import UserContext from "../../context/UserContext";
@@ -124,6 +124,9 @@ export default function EditUser({ mode }) {
                         "Content-Type": "multipart/form-data",
                     },
                 });
+
+                addUserToCache(user._id, null);
+
                 if (mode === "self") {
                     navigate("/success");
                 } else {
@@ -136,7 +139,7 @@ export default function EditUser({ mode }) {
     }
 
     return userExists ? (
-        <div className="d-flex justify-content-center">
+        <div className="d-flex justify-content-center mb-3">
             <div className="w-50">
                 <h2 className="text-center mb-3">{"Edit: " + user.username}</h2>
                 {error && <p className="text-center text-danger">{error}</p>}
@@ -259,7 +262,31 @@ export default function EditUser({ mode }) {
                             </div>
                         </div>
                     )}
-                    <button className="btn btn-primary mb-3">Submit</button>
+                    <div className="d-flex justify-content-between">
+                        <button className="btn btn-primary">Submit</button>
+                        {mode === "admin" && (
+                            <div className="btn-group">
+                                <Link
+                                    className="btn btn-outline-danger"
+                                    to={"/manageusers/delete/" + user._id}
+                                >
+                                    {/* https://icons.getbootstrap.com/icons/trash/ */}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        width="16"
+                                        height="16"
+                                        fill="currentColor"
+                                        className="bi bi-trash me-1"
+                                        viewBox="0 0 16 16"
+                                    >
+                                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                                    </svg>
+                                    Delete User
+                                </Link>
+                            </div>
+                        )}
+                    </div>
                 </form>
             </div>
         </div>
